a3291b3f6502b1621c1aaeb7e09e0014f7d5c9f2
BHIM RAJBHAR <62300815+Bhim-Rajbhar@users.noreply.github.com>
2024-01-27 21:46:35 +0530

    Update BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -2,24 +2,26 @@
 ################ BMI Calculator ######################
 
 '''
-
 BMI full form is Body Mass Index.
 Formula for calculating BMI :
 
 BMI = Weight / Height ** 2 
 or
 BMI = kg/m2
-
 '''
+
 
 print("Welcome to Bhim's BMI Calculator\n")
 
+# Getting user input for weight and height
 Weight = float(input("Enter your Weight in KG : ")) # Using Input Fuction
-
 Height = float(input("Enter your Height in M : ")) # Using Input Fuction
 
-BMI = Weight / Height ** 2 # formula for calculating BMI
+# Calculating BMI using the formula
+BMI = Weight / Height ** 2 
 
-BMI = round(BMI, 2) # Using round function
+# Rounding the BMI to two decimal places
+BMI = round(BMI, 2) 
 
+# Printing the calculated BMI
 print(BMI)
